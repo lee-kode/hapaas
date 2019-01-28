ee17828dd74fd7fa39e32fa9c0a9d7107031aaba
lee-kode <lee@kode.co.za>
2019-01-28 23:50:26 +0200

    wip

--- api/api.py
+++ api/api.py
@@ -1,19 +1,26 @@
-from flask import Flask
+from flask import Flask, json, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/api/v1/frontend')
 def handle_frontend():
-    return 'Hello, frontend!'
+    return jsonify(message="Hello, frontend!")
 
 
 @app.route('/api/v1/backend')
 def handle_backend():
-    return 'Hello, backend!'
+    return jsonify(message="Hello, backend!")
 
 
 @app.route('/api/v1/listen')
 def handle_listen():
-    return 'Hello, listen!'
+    return jsonify(message="Hello, listen!")
 
+
+if __name__ == '__main__':
+    # TODO: This must get pulled from an config object
+    host = '127.0.0.1'
+    port = 8080
+
+    app.run(host=host, port=port)
