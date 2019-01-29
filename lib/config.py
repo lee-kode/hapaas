b4243ce8887043adacecbc017de75b74ef7d53b8
lee_engel <lee.engel@oracle.com>
2019-01-29 16:14:33 +0200

    wip

--- lib/config.py
+++ lib/config.py
@@ -1,12 +1,14 @@
+
+import ConfigParser
 import json
 import os
 
 # TODO: we need to put some defaults here at some point
 
-CONFIG_FILE = '/etc/hapaas/config.json'
+CONFIG_FILE = '/etc/hapaas/config.ini'
 
 
 def get_config():
     cfg_file = os.getenv('HAPAAS_CONFIG', CONFIG_FILE)
-    with open(cfg_file) as cfh:
-        return json.load(cfh)
+    config = ConfigParser.RawConfigParser()
+    return config.read(cfg_file)
